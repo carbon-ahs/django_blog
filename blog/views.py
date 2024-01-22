83c32f808c6a222c019c6e2fc21fe541f57d1e7e
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-01-22 11:58:16 +0600

    Mediam like page title added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
@@ -7,5 +7,23 @@ from blog.models import Post
 # Create your views here.
 class BlogListView(ListView):
     model = Post
+    queryset = Post.objects.order_by("-created_at")
+    context_object_name = "post_list"
     template_name = "home.html"
-    # template_name = "base.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = "Home"
+        return context
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs["pk"]
+        title = Post.objects.get(pk=pk).title
+        context["page_title"] = title
+        return context
